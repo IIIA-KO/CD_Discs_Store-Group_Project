0fb88f5d83ecef440a390c062e2698c6680ffe18
Illia Kotviskyi <iiiako197@gmail.com>
2023-12-19 15:53:50 +0200

    Client CRUD

---
+++ CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Data/Models/Client.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using CD_Disc_Store_React_ASP_NET_Core.Server.Utilities.Atributes;
+
+namespace CD_Disc_Store_React_ASP_NET_Core.Server.Data.Models
+{
+    [Table("Client")]
+    public class Client
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "The \"First name\" field is required")]
+        public string FirstName { get; set; } = default!;
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "The \"Last name\" field is required")]
+        public string LastName { get; set; } = default!;
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "The \"Address\" field is required")]
+        public string Address { get; set; } = default!;
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "The \"City\" field is required")]
+        public string City { get; set; } = default!;
+
+        [StringLength(20)]
+        [Required(ErrorMessage = "The \"Contact phone\" field is required")]
+        [PhoneValidation]
+        public string ContactPhone { get; set; } = default!;
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "The \"Contact mail\" field is required")]
+        [EmailAddressValidation]
+        public string ContactMail { get; set; } = default!;
+
+        [Required(ErrorMessage = "The \"Birth day\" field is required")]
+        public DateTime BirthDay { get; set; }
+
+        public bool MarriedStatus { get; set; } = false;
+
+        public bool Sex { get; set; } = false;
+
+        public bool HasChild { get; set; } = false;
+    }
+}
---
+++ CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Utilities/Exstensions/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Contexts;
+using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Repositories.Implementations;
+using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Repositories.Interfaces;
+
+namespace CD_Disc_Store_React_ASP_NET_Core.Server.Utilities.Exstensions
+{
+    public static class ServiceRegistration
+    {
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton<IDapperContext, DapperContext>();
+
+            services.AddScoped<IClientRepository, ClientRepository>();
+        }
+    }
+}
