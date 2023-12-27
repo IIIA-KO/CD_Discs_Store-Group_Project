10ea0eff0e3bc70e581fdd2c2d9ad26c483aae9a
Illia Kotviskyi <iiiako197@gmail.com>
2023-12-27 16:10:15 +0200

    Added Identity

---
+++ CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CD_Disc_Store_React_ASP_NET_Core.Server.Data.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            { }
+    }
+}
--- CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Utilities/Exstensions/ServiceRegistration.cs
+++ CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Utilities/Exstensions/ServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Contexts;
 using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Repositories.Implementations;
 using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace CD_Disc_Store_React_ASP_NET_Core.Server.Utilities.Exstensions
 {
@@ -14,6 +16,22 @@ namespace CD_Disc_Store_React_ASP_NET_Core.Server.Utilities.Exstensions
             services.AddScoped<IMusicRepository, MusicRepository>();
             services.AddScoped<IDiscRepository, DiscRepository>();
             services.AddScoped<IFilmRepository, FilmRepositiry>();
+        }
+
+        public static void RegisterIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityCore<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddApiEndpoints();
+
+            services.AddAuthentication();
         }
     }
 }
