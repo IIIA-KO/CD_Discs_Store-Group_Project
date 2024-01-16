8c12e29d2fb27c2a00221c9fde9a5560df408389
DarynaArkhypchuk <darinka.ark2000@gmail.com>
2024-01-16 18:07:54 +0200

    Footer and Rating

--- CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Data/Models/Film.cs
+++ CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Data/Models/Film.cs
@@ -1,11 +1,13 @@
 using CD_Disc_Store_React_ASP_NET_Core.Server.Utilities.Atributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 
 namespace CD_Disc_Store_React_ASP_NET_Core.Server.Data.Models
 {
-    [Table("Disc")]
-    public class Disc
+    [Table("Film")]
+    public class Film
     {
         [Key]
         public Guid Id { get; set; }
@@ -14,24 +16,20 @@ namespace CD_Disc_Store_React_ASP_NET_Core.Server.Data.Models
         [Required(ErrorMessage = "The \"Name\" field is required")]
         public string Name { get; set; } = default!;
 
-        [Required(ErrorMessage = "The \"Price\" field is required")]
-        public decimal Price { get; set; } = default!;
+        [StringLength(50)]
+        [Required(ErrorMessage = "The \"Genre\" field is required")]
+        public string Genre { get; set; } = default!;
 
-        [Range(0, int.MaxValue)]
-        [Required(ErrorMessage = "The \"Left On Stock\" field is required")]
-        public int LeftOnStock { get; set; } = default!;
+        [StringLength(50)]
+        [Required(ErrorMessage = "The \"Producer\" field is required")]
+        public string Producer { get; set; } = default!;
 
-        [Range(0, 5)]
-        [Required(ErrorMessage = "The \"Rating\" field is required")]
-        public decimal Rating { get; set; } = default!;
+        [StringLength(50)]
+        [Required(ErrorMessage = "The \"Main Role\" field is required")]
+        public string MainRole { get; set; } = default!;
 
-        [StringLength(250)]
-        public string? CoverImagePath { get; set; }
-
-        public string? ImageStorageName { get; set; }
-        
-        [MaxFileSize(1 * 1024 * 1024)]
-        [AllowedFileExtensions(new string[] { ".jpg", ".png", ".jpeg"})]
-        public IFormFile? ImageFile { get; set; }
+        [Range(0, 18)]
+        [Required(ErrorMessage = "The \"Age Limit\" field is required")]
+        public int AgeLimit { get; set; }
     }
 }
--- CD-Disc-Store_React_ASP-NET-Core/cd-disc-store_react_asp-net-core.client/src/pages/Home/Home.jsx
+++ CD-Disc-Store_React_ASP-NET-Core/cd-disc-store_react_asp-net-core.client/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Stars from '../../Stars/Stars'
 
 const Home = () => {
   return (
     <div>
       <p>home</p>
+      <Stars rating='3'/>
     </div>
   )
 }
