d43d90518fa83e11ce7c02c184abf141e6e1bfa5
Illia Kotviskyi <iiiako197@gmail.com>
2024-01-21 15:13:21 +0200

    Added getting films and music from disc

--- CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Data/Repositories/Implementations/DiscRepository.cs
+++ CD-Disc-Store_React_ASP-NET-Core/CD-Disc-Store_React_ASP-NET-Core.Server/Data/Repositories/Implementations/DiscRepository.cs
@@ -1,4 +1,4 @@
-﻿using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Contexts;
+using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Contexts;
 using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Models;
 using CD_Disc_Store_React_ASP_NET_Core.Server.Data.Repositories.Interfaces;
 using CD_Disc_Store_React_ASP_NET_Core.Server.Utilities.Exceptions;
@@ -103,6 +103,48 @@ namespace CD_Disc_Store_React_ASP_NET_Core.Server.Data.Repositories.Implementati
         public Task<int> CountProcessedDataAsync(string? searchText)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<Film>> GetFilmsOnDiscAsync(Guid? id)
+        {
+            if (id is null)
+            {
+                throw new NullReferenceException(DISC_NOT_FOUND_BY_ID_ERROR);
+            }
+
+            using IDbConnection dbConnection = this._context.CreateConnection();
+
+            var query = @"
+                SELECT
+                    Film.*
+                FROM
+                    Disc
+                JOIN DiscFilm ON Disc.Id = DiscFilm.IdDisc
+                JOIN Film ON DiscFilm.IdFilm = Film.Id
+                WHERE Disc.Id = @DiscId;";
+
+            return (IReadOnlyList<Film>)await dbConnection.QueryAsync<Film>(query, new { DiscId = id });
+        }
+
+        public async Task<IReadOnlyList<Music>> GetMusicOnDiscAsync(Guid? id)
+        {
+            if (id is null)
+            {
+                throw new NullReferenceException(DISC_NOT_FOUND_BY_ID_ERROR);
+            }
+
+            using IDbConnection dbConnection = this._context.CreateConnection();
+
+            var query = @"
+                SELECT
+                    Music.*
+                FROM
+                    Disc
+                JOIN DiscMusic ON Disc.Id = DiscMusic.IdDisc
+                JOIN Music ON DiscMusic.IdMusic = Music.Id
+                WHERE Disc.Id = @DiscId;";
+
+            return (IReadOnlyList<Music>)await dbConnection.QueryAsync<Music>(query, new { DiscId = id });
         }
     }
 }
